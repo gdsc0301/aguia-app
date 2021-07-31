2e6ab3d2208702a4d6ec8064a46b23639dd39688
Guilherme Carvalho <gdsc0301@gmail.com>
2021-07-31 02:11:33 -0300

    Release 0.2

--- src/pages/qsp/test-generator/TestGenerator.js
+++ src/pages/qsp/test-generator/TestGenerator.js
@@ -1,5 +1,5 @@
 import { Box, TextField, MenuItem, Grid, Paper, Button, Fab } from "@material-ui/core";
-import { Add, Print } from "@material-ui/icons";
+import { Add, Remove, Print } from "@material-ui/icons";
 
 import React from "react";
 
@@ -19,9 +19,7 @@ const styles = {
     },
     testPage: {
         width: "210mm",
-        height: "297mm",
-        padding:"1cm 2cm",
-        objectFit: "contain",
+        padding: "1cm 2cm",
         transformOrigin: "top left",
         transform: "scale(.75)",
         chapter: {
@@ -34,17 +32,17 @@ const styles = {
             transform: "translate(50%, -50%)"
         },
         questions: {
-            display: "flex",
-            flexDirection: "column",
-            flexWrap: "wrap",
-            maxHeight: "80%",
+            display: "block",
             marginTop: "32px",
             columnCount: 3,
             question: {
+                display: "inline-block",
+                width: "100%",
                 breakInside: "avoid",
                 breakBefore: "column",
                 text: {
-                    marginTop: 0
+                    margin: 0,
+                    marginTop: 4
                 },
                 alternatives: {
                     listStyle: "lower-alpha"
@@ -55,6 +53,9 @@ const styles = {
     testOptions: {
         question: {
             input: {
+                width: "100%",
+            },
+            alternative: {
                 width: "100%",
                 margin: "12px 0"
             }
@@ -84,6 +85,7 @@ export class TestGenerator extends React.Component {
         this.bindInput = this.bindInput.bind(this);
         this.updateTest = this.updateTest.bind(this);
         this.addQuestion = this.addQuestion.bind(this);
+        this.removeQuestion = this.removeQuestion.bind(this);
     }
 
     storeState() {
@@ -139,36 +141,47 @@ export class TestGenerator extends React.Component {
         });
     }
 
+    removeQuestion(chapter, question) {
+        this.setState((curr)=>{
+            console.log(curr);
+            delete curr.bookChapters[chapter][question];
+            return curr;
+        });
+    }
+
     render(){
+        let questionNumber = 0;
+
         return (
             <Box className="testContainer" style={{marginTop: 32}}>
                 <Grid container spacing={4} style={styles.testOptions}>
                     <Grid item lg={6}>
                         <h3>Adicionar pergunta</h3>
-                        <form id="newQuestion" style={styles.testOptions.question} onSubmit={this.addQuestion}>
-                            <Grid container spacing={4}>
-                                <Grid item lg={9}>
-                                    <TextField onInput={this.bindInput} style={{...styles.testOptions.question.input, marginBottom: 16}} variant="outlined" name="question" label="Insira a pergunta" multiline required />
-                                </Grid>
-                                <Grid item lg={3}>
-                                    <TextField onInput={this.bindInput} style={{...styles.testOptions.question.input, marginBottom: 16}} variant="outlined" name="questionChapter" label="Capítulo" type="number" required />
-                                </Grid>
+                        <Grid container spacing={4}>
+                            <Grid item md={8}>
+                                <TextField onInput={this.bindInput} style={{...styles.testOptions.question.input, marginBottom: 16}} variant="outlined" name="question" label="Insira a pergunta" multiline required />
                             </Grid>
-                            
-                            <TextField style={styles.testOptions.question.input} onInput={this.bindInput} name="A" placeholder='Resposta "A"' required/>
-                            <TextField style={styles.testOptions.question.input} onInput={this.bindInput} name="B" placeholder='Resposta "B"' required/>
-                            <TextField style={styles.testOptions.question.input} onInput={this.bindInput} name="C" placeholder='Resposta "C"' required/>
-                            <TextField style={styles.testOptions.question.input} onInput={this.bindInput} name="D" placeholder='Resposta "D"' required/>
-                            <Grid container alignItems="center">
-                                <Grid item>
-                                    <Button
-                                        color="primary"
-                                        variant="contained"
-                                        type="submit"
-                                        endIcon={<Add />}>Inserir nova Pergunta</Button>
-                                </Grid>
+                            <Grid item md={3}>
+                                <TextField onInput={this.bindInput} style={{...styles.testOptions.question.input, marginBottom: 16}} variant="outlined" name="questionChapter" label="Capítulo" type="number" required />
                             </Grid>
-                        </form>
+                        </Grid>
+                        <Grid container spacing={4}>
+                            <Grid item lg={12}>
+                                <TextField style={styles.testOptions.question.alternative} onInput={this.bindInput} name="A" placeholder='Resposta "A"' required/>
+                                <TextField style={styles.testOptions.question.alternative} onInput={this.bindInput} name="B" placeholder='Resposta "B"' required/>
+                                <TextField style={styles.testOptions.question.alternative} onInput={this.bindInput} name="C" placeholder='Resposta "C"' required/>
+                                <TextField style={styles.testOptions.question.alternative} onInput={this.bindInput} name="D" placeholder='Resposta "D"' required/>
+                            </Grid>
+                        </Grid>
+                        <Grid container alignItems="center">
+                            <Grid item>
+                                <Button
+                                    color="primary"
+                                    variant="contained"
+                                    type="submit"
+                                    endIcon={<Add />}>Inserir nova Pergunta</Button>
+                            </Grid>
+                        </Grid>
                     </Grid>
                     <Grid item>
                         <h3>Informações da Prova</h3>
@@ -233,20 +246,25 @@ export class TestGenerator extends React.Component {
                             </Grid>
                         </Grid>
                     </Grid>
-                    <Box style={styles.testPage.questions}>
+                    <Box className="questions" style={styles.testPage.questions}>
                         {
-                            this.state.bookChapters.map((chapterQuestions, i) => {
+                            this.state.bookChapters.map((chapterQuestions, chapterNumber) => {
                                 let chapter = undefined;
                                 if(chapterQuestions){
                                     chapter = (
                                         <>
-                                            <h4 style={styles.testPage.chapter}>Capítulo {i}</h4>
+                                            <h4 style={styles.testPage.chapter}>Capítulo {chapterNumber}</h4>
                                             {
                                                 chapterQuestions.map(
-                                                    (question, i) => {
+                                                    (question, questionIndex) => {
+                                                        questionNumber++;
+
                                                         return (
                                                             <Box style={styles.testPage.questions.question}>
-                                                                <h5 style={styles.testPage.questions.question.text}>{++i}) {question.question}</h5>
+                                                                <Fab className="noPrint" chapter={chapterNumber} question={questionIndex} style={styles.testPage.questions.removeButton} color="primary" onClick={this.removeQuestion} aria-label="print">
+                                                                    <Remove />
+                                                                </Fab>
+                                                                <h5 style={styles.testPage.questions.question.text}>{questionNumber}) {question.question}</h5>
                                                                 <ol style={styles.testPage.questions.question.alternatives}>
                                                                     <li>{question.A}</li>
                                                                     <li>{question.B}</li>
